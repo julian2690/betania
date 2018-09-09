371eab02d8dc130822b597765affb49e38684450
kallu <kallu08@hotmail.com>
2018-09-09 15:32:35 -0600

    primer commit de mi betania

---
+++ lideres/js-lideres/lideres.js
@@ -0,0 +1,44 @@
+//ERLEY
+$(window).scroll(function(){
+        if($(this).scrollTop() > 100) {
+            $(".pastor-erley").animate({
+                "opacity": "1"
+            },1000);
+        }
+});
+
+//DAVID
+$(window).scroll(function(){
+        if($(this).scrollTop() > 400) {
+            $(".pastor-david").animate({
+                "opacity": "1"
+            },1000);
+        }
+});
+
+//ZAIDA
+$(window).scroll(function(){
+        if($(this).scrollTop() > 900) {
+            $(".maestra-zaida").animate({
+                "opacity": "1"
+            },1000);
+        }
+});
+
+//MARIA
+$(window).scroll(function(){
+        if($(this).scrollTop() > 1300) {
+            $(".maestra-maria").animate({
+                "opacity": "1"
+            },1000);
+        }
+});
+
+//MARCELLY
+$(window).scroll(function(){
+        if($(this).scrollTop() > 1700) {
+            $(".musica-marcelly").animate({
+                "opacity": "1"
+            },1000);
+        }
+});
---
+++ lideres/js-lideres/main.js
@@ -0,0 +1,176 @@
+//BOTON IR ARRIBA
+    $(".ir-arriba").click(function(){
+       $("body,html").animate({
+           "scrollTop": "0px"
+       },350); 
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 500) {
+            $(".ir-arriba").fadeIn(300);
+        } else {
+            $(".ir-arriba").fadeOut(300);
+        }
+    });
+
+
+
+
+
+//QUIENES SOMOS
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 250) {
+            $(".section-presentacion h2").animate({
+                "opacity": "1",
+                "top": "0"
+            },1500);
+        }
+    });
+
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 250) {
+            $(".section-presentacion p").animate({
+                "opacity": "1"
+            },1500);
+        }
+    });
+
+
+//VERSICULOS
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 900) {
+            $("section.section-versiculo .versiculo p").animate({
+                "opacity": "1",
+                "top": "0"
+            },1500);
+        }
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 900) {
+            $("section.section-versiculo .versiculo q").animate({
+                "opacity": "1"
+            },4000);
+        }
+    });
+
+
+//SECTION-LIDERES
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1500) {
+            $("section.section-lideres h3 span").animate({
+                "opacity": "1",
+                "top": "0"
+            },1500);
+        }
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1500) {
+            $("section.section-lideres .c-lideres #lider-uno").animate({
+                "opacity": "1",
+                "left": "0"
+            },1800);
+        }
+    });
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1500) {
+            $("section.section-lideres .c-lideres #lider-dos").animate({
+                "opacity": "1",
+                "left": "0"
+            },2100);
+        }
+    });
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1500) {
+            $("section.section-lideres .c-lideres #lider-tres").animate({
+                "opacity": "1",
+                "left": "0"
+            },2400);
+        }
+    });
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1500) {
+            $("section.section-lideres .c-lideres #lider-cuatro").animate({
+                "opacity": "1",
+                "left": "0"
+            },2700);
+        }
+    });
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1500) {
+            $("section.section-lideres .c-lideres #lider-cinco").animate({
+                "opacity": "1",
+                "left": "0"
+            },3000);
+        }
+    });
+
+
+/*MENU RESPONSIVE*/ 
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+ 
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
